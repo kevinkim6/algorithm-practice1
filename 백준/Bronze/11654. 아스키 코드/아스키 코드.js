3d23ee5be523c701e5556d4f3e462e42e2cd163f
kevinkim6 <109018549+kevinkim6@users.noreply.github.com>
2022-12-31 04:20:50 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+let answer = input[0].charCodeAt();
+
+console.log(answer);
