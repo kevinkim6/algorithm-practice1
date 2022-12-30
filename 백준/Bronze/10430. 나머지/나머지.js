83da2d0703102ab39d29b03f10fe77438434ff15
kevinkim6 <109018549+kevinkim6@users.noreply.github.com>
2022-12-31 04:10:02 +0900

    [Bronze V] Title: 나머지, Time: 116 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0];
+input = input.split(' ');
+input = input.map((item) => +item);
+
+let A = input[0];
+let B = input[1];
+let C = input[2];
+
+console.log((A+B)%C);
+console.log(((A%C)+(B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C)*(B%C))%C);
