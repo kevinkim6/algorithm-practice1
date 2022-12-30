1da8f7e846407a2ab93a044220e3adbb49f10359
kevinkim6 <109018549+kevinkim6@users.noreply.github.com>
2022-12-31 04:17:20 +0900

    [Bronze V] Title: A+B - 7, Time: 136 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+let A = Number(input[0]);
+
+for(i = 1; i <= A; i++) {
+    let [B, C] = input[i].split(' ');
+    console.log("Case #"+i+":",(Number(B)+Number(C)));
+}
