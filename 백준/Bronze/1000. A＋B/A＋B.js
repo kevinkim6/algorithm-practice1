619ad6d30bb196c29ffe9647a668a843edbdcca1
kevinkim6 <109018549+kevinkim6@users.noreply.github.com>
2022-12-31 04:07:55 +0900

    [Bronze V] Title: A+B, Time: 120 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0];
+input = input.split(' ');
+
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+
+console.log(A + B);
