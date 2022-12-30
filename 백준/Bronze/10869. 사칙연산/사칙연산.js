52defa18991f44736ae40714bad427d5b52ff036
kevinkim6 <109018549+kevinkim6@users.noreply.github.com>
2022-12-31 04:09:18 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0]
+input = input.split(' ');
+
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(Math.floor(A/B));
+console.log(A%B);
